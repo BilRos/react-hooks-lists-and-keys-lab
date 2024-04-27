872481f06e27140107b3aa6edab69293227fab47
BilRos <billyrossowuor@gmail.com>
2024-04-27 14:33:38 +0300

    Used some list and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const NavBar1 = links.map((head)=>{
+  return <a key={head} href={'#' + head}>{head}</a>
+  }
+);
+  return <nav> {NavBar1} </nav>
+  
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <span>{technologies.map((tech) => {
+          return <span key={tech}>{tech}</span>
+      })
+    }
+  </span>
       </div>
     </div>
   );
